977e63522ebdfcdf1132fa86b6484eb7b3b64468
devpin95 <devpindroid@gmail.com>
2021-01-13 02:18:42 -0700

    made function to run in thread, added config reading w/ lock

--- CaptureScreen.py
+++ CaptureScreen.py
@@ -3,9 +3,23 @@ from PIL import Image
 import keyboard
 import win32clipboard as cb
 from io import BytesIO
+from filelock import FileLock
+import json
+from datetime import datetime
 
 NUM_MONITORS = 3
 active_monitor = 1
+CONFIG_LOCK_FILE = 'config.lock'
+CONFIG_FILE = 'config.json'
+CONFIG_DICT = {}
+FLOCK = FileLock(CONFIG_LOCK_FILE)
+
+
+def get_config():
+    global CONFIG_DICT
+    with FLOCK:
+        with open(CONFIG_FILE, 'r') as fp:
+            CONFIG_DICT = json.load(fp)
 
 
 def send_to_clipboard(clip_type, data):
@@ -22,30 +36,37 @@ def set_active_monitor(mid):
 
 
 def on_trigger():
+
+    get_config()
+
+    print(CONFIG_DICT)
+
     with mss.mss() as scr:
+        print("Taking sss")
         scr_img = scr.grab(scr.monitors[active_monitor])
 
         img = Image.frombytes("RGB", scr_img.size, scr_img.bgra, "raw", "BGRX")
-        img.save('screenshot.png')
+
+        if CONFIG_DICT['save_ss']:
+            path = '{}/{}-screenshot.png'.format(CONFIG_DICT['ss_path'], datetime.now().strftime("%m%d%Y-%H%M%S"))
+            print("PATH", path)
+            img.save(path)
 
         output = BytesIO()
         img.save(output, "BMP")
         data = output.getvalue()[14:]
         output.close()
 
-        send_to_clipboard(cb.CF_DIB, data)
-
+        if CONFIG_DICT['copy_2_cb']:
+            send_to_clipboard(cb.CF_DIB, data)
 
 switch_screen_shortcuts = ['ctrl+alt+' + str(i) for i in range(0, NUM_MONITORS)]
 for sc in switch_screen_shortcuts:
     keyboard.add_hotkey(sc, lambda sc=sc: set_active_monitor(sc[-1]))
 
-
-print(switch_screen_shortcuts)
-
 shortcut = "ctrl+alt+s"
 keyboard.add_hotkey(shortcut, on_trigger)
 
-print("Ready to capture...")
-keyboard.wait()
+def start_listening_for_hotkeys():
+    keyboard.wait()
 
