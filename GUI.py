658a48321d815feb9f1758313ca4d67797611945
devpin95 <devpindroid@gmail.com>
2021-01-17 20:45:26 -0700

    added field to change hotkey, added event filter to capture keyboard input while changing hotkey

--- GUI.py
+++ GUI.py
@@ -1,6 +1,6 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QWidget, QVBoxLayout, QCheckBox, QHBoxLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QPushButton, QFileDialog, QWidget, QVBoxLayout, QCheckBox, QHBoxLayout, QScrollArea, QAbstractScrollArea
 from PyQt5.QtGui import QFont, QCursor, QIcon
-from PyQt5 import QtCore, QtWinExtras
+from PyQt5 import QtCore
 import sys
 import mss
 from PIL import Image
@@ -11,6 +11,8 @@ from filelock import FileLock
 import json
 import ctypes
 import os
+import keyboard
+from QtKeyValueToKeyboardValue import qt_to_keyboard
 
 # https://stackoverflow.com/questions/1551605/how-to-set-applications-taskbar-icon-in-windows-7/1552105#1552105
 # for setting the taskbar icon in windows
@@ -26,12 +28,18 @@ THUMBNAIL_HEIGHT = 281
 GLOBAL_DICT = {"hello": "World!"}
 DICT_LOCK = RLock()
 
+APP_WIDTH = 550
+APP_HEIGHT = 800
+WIDGET_WIDTH = 500
+FIELD_HEIGHT = 30
+
 WINDOW_TITLE = 'PowerSS'
 CONFIG_FILE = 'config.json'
 CONFIG_LOCK_FILE = 'config.lock'
 CONFIG_DICT = {}
 FLOCK = FileLock(CONFIG_LOCK_FILE)
 
+key_list = []
 
 def start_gui():
     sys.exit(app.exec_())
@@ -43,13 +51,48 @@ def update_config():
             json.dump(CONFIG_DICT, fp)
 
 
+class GetHotkeyPopup(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.label = QLabel('Press esc to cancel\nPress enter to accept new hotkey')
+        self.label.show()
+        self.setFixedWidth(100)
+        self.setFixedHeight(100)
+
+    def mycallback_release(self, e):
+        pass
+
+    def mycallback_press(self, e):
+        global key_list
+        if e.name == 'esc':
+            key_list = []
+
+        if e.name not in key_list:
+            key_list.append(e.name)
+            hks = ''
+            for i in range(0, len(key_list) - 1):
+                hks += key_list[i] + '+'
+
+            hks += key_list[-1]
+            print(hks)
+
+    def get_hotkey(self):
+        global key_list
+
+        keyboard.on_press(self.mycallback_press)
+        keyboard.on_press(self.mycallback_release)
+
+        keyboard.wait('esc')
+
+
 class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
         # self.setStyleSheet("padding: 5px 10px 5px 10px")
-        self.setFixedWidth(525)
-        self.setFixedHeight(800)
+        self.setFixedWidth(APP_WIDTH)
+        self.setFixedHeight(APP_HEIGHT)
         self.setWindowTitle(WINDOW_TITLE)
 
         # self.setWindowIcon(app_icon)
@@ -58,11 +101,12 @@ class Window(QMainWindow):
 
         main_layout = QVBoxLayout()
         self.main_widget = QWidget(self)
-        self.main_widget.setFixedWidth(525)
-        self.main_widget.setFixedHeight(800)
+        self.main_widget.setFixedWidth(APP_WIDTH)
+        self.main_widget.setFixedHeight(APP_HEIGHT)
 
+        # Instruction --------------------------------------------------------------------------------------------------
         self.g_instructions = QWidget(self)
-        self.g_instructions.setFixedWidth(500)
+        self.g_instructions.setFixedWidth(WIDGET_WIDTH)
         self.g_instructions.setFixedHeight(100)
         self.g_instructions.setStyleSheet('border: 1px solid #000;'
                                           'border-radius: 2px')
@@ -76,7 +120,6 @@ class Window(QMainWindow):
         self.g_instructions_title.setStyleSheet('border: none;')
         g_instructions_layout.addWidget(self.g_instructions_title)
 
-        # To see which monitor is which, test the preview below
         self.g_instructions_hotkeys = QLabel('ctrl+alt+[monitor number] - Set active monitor\nctrl+alt+s - Take screenshot')
         self.g_instructions_hotkeys.setFont(QFont('Arial', 10))
         self.g_instructions_hotkeys.setStyleSheet('border: none;')
@@ -90,14 +133,67 @@ class Window(QMainWindow):
         g_instructions_layout.addStretch()
         self.g_instructions.setLayout(g_instructions_layout)
 
+        # Settings -----------------------------------------------------------------------------------------------------
         self.g_settings = QWidget(self)
-        self.g_settings.setFixedWidth(500)
+        self.g_settings.setFixedWidth(WIDGET_WIDTH)
         self.g_settings.setFixedHeight(250)
+        self.g_settings.setStyleSheet('padding: 0; margin: 0;')
+        # self.g_settings.setStyleSheet('QWidget{background-color:#f00;} QWidget QCheckBox {padding: 0; margin: 0; background-color: #0f0;}')
 
         g_settings_layout = QVBoxLayout()
+        g_settings_layout.setContentsMargins(0, 0, 0, 0)
 
+        # Hotkey -------------------------------------------------------------------------------------------------------
+        self.g_settings_set_hotkey_label = QLabel('Capture Hotkey', self)
+        self.g_settings_set_hotkey_label.setFixedHeight(FIELD_HEIGHT)
+        self.g_settings_set_hotkey_label.setFont(QFont('Arial', 10))
+        self.g_settings_set_hotkey_label.setStyleSheet("border: none;")
+        g_settings_layout.addWidget(self.g_settings_set_hotkey_label)
+
+        self.g_settings_set_hotkey = QWidget(self)
+        self.g_settings_set_hotkey.setFixedHeight(30)
+        self.g_settings_set_hotkey.setFixedWidth(WIDGET_WIDTH)
+        self.g_settings_set_hotkey.setStyleSheet('QLabel{'
+                                                 'padding-left: 5px;'
+                                                 'border: 1px solid #bbb;'
+                                                 'border-radius: 2px;}')
+
+        hotkey_layout = QHBoxLayout()
+        hotkey_layout.setContentsMargins(0, 0, 0, 0)
+
+        self.g_settings_hotkey_val = QLabel(CONFIG_DICT['ss_hotkey'], self)
+        self.g_settings_hotkey_val.setFixedHeight(FIELD_HEIGHT)
+
+        hotkey_layout.addWidget(self.g_settings_hotkey_val)
+
+        self.g_setting_hotkey_change_button = QPushButton("Set")
+        self.g_setting_hotkey_change_button.setFixedHeight(FIELD_HEIGHT)
+        # self.g_setting_hotkey_change_button.setFixedWidth(100)
+        self.g_setting_hotkey_change_button.setStyleSheet('QPushButton {'
+                                                          'border-radius: 2px;'
+                                                          'background-color: qlineargradient('
+                                                          'x1: 0, y1: 0, x2: 0, y2: 1,'
+                                                          'stop: 0 #fff, stop: 1 #eee); '
+                                                          'border: 1px solid #bbb;'
+                                                          '} '
+                                                          'QPushButton:pressed { '
+                                                          'background-color: qlineargradient('
+                                                          'x1: 0, y1: 0, x2: 0, y2: 1,'
+                                                          'stop: 0 #eee, stop: 1 #fff); '
+                                                          '} '
+                                                          'QPushButton:hover {'
+                                                          'border: 1px solid #666;'
+                                                          '}')
+        self.g_setting_hotkey_change_button.setCursor(QCursor(QtCore.Qt.PointingHandCursor))
+        self.g_setting_hotkey_change_button.clicked.connect(self.get_custom_hotkey)
+        hotkey_layout.addWidget(self.g_setting_hotkey_change_button)
+
+        self.g_settings_set_hotkey.setLayout(hotkey_layout)
+        g_settings_layout.addWidget(self.g_settings_set_hotkey)
+
+        # Save SS Checkbox ---------------------------------------------------------------------------------------------
         self.g_settings_save_file_cb = QCheckBox('Save Screenshot', self)
-        self.g_settings_save_file_cb.setFixedHeight(30)
+        self.g_settings_save_file_cb.setFixedHeight(FIELD_HEIGHT)
         self.g_settings_save_file_cb.setFont(QFont('Arial', 10))
         self.g_settings_save_file_cb.setStyleSheet("border: none;")
         self.g_settings_save_file_cb.stateChanged.connect(self.cb_save_file_state_changed)
@@ -106,8 +202,10 @@ class Window(QMainWindow):
         if CONFIG_DICT['ss_path'] == '':
             self.init_save_path()
 
+        # SS Path ------------------------------------------------------------------------------------------------------
         self.g_settings_save_file_path = QLabel("Path: {}".format(CONFIG_DICT['ss_path']), self)
-        self.g_settings_save_file_path.setFixedHeight(30)
+        self.g_settings_save_file_path.setFixedHeight(FIELD_HEIGHT)
+        # self.g_settings_save_file_path.setFixedWidth(WIDGET_WIDTH)
         self.g_settings_save_file_path.setStyleSheet('QLabel{ padding-left: 5px;'
                                                      'border: 1px solid #bbb; '
                                                      'border-radius: 2px;'
@@ -122,36 +220,39 @@ class Window(QMainWindow):
         g_settings_layout.addWidget(self.g_settings_save_file_cb)
         g_settings_layout.addWidget(self.g_settings_save_file_path)
 
+        # Copy to clipboard cb -----------------------------------------------------------------------------------------
         self.g_settings_cpy2_clip = QCheckBox('Copy to Clipboard', self)
-        self.g_settings_cpy2_clip.setFixedHeight(40)
+        self.g_settings_cpy2_clip.setFixedHeight(FIELD_HEIGHT)
         self.g_settings_cpy2_clip.setFont(QFont('Arial', 10))
         self.g_settings_cpy2_clip.setStyleSheet('border: none;')
         self.g_settings_cpy2_clip.stateChanged.connect(self.cb_cpy2_clip_state_changed)
         self.g_settings_cpy2_clip.setChecked(CONFIG_DICT['copy_2_cb'])
         g_settings_layout.addWidget(self.g_settings_cpy2_clip)
 
+        # Show toast on monitor change ---------------------------------------------------------------------------------
         self.g_settings_show_toast_on_monitor_change = QCheckBox('Toast on Monitor Change', self)
-        self.g_settings_show_toast_on_monitor_change.setFixedHeight(40)
+        self.g_settings_show_toast_on_monitor_change.setFixedHeight(FIELD_HEIGHT)
         self.g_settings_show_toast_on_monitor_change.setFont(QFont('Arial', 10))
         self.g_settings_show_toast_on_monitor_change.setStyleSheet('border: none;')
         self.g_settings_show_toast_on_monitor_change.stateChanged.connect(self.cb_show_toast_on_monitor_state_changed)
         self.g_settings_show_toast_on_monitor_change.setChecked(CONFIG_DICT['show_toast_on_monitor_change'])
         g_settings_layout.addWidget(self.g_settings_show_toast_on_monitor_change)
 
+        # Show toast on capture ----------------------------------------------------------------------------------------
         self.g_settings_show_toast_on_capture = QCheckBox('Toast on Capture', self)
-        self.g_settings_show_toast_on_capture.setFixedHeight(40)
+        self.g_settings_show_toast_on_capture.setFixedHeight(FIELD_HEIGHT)
         self.g_settings_show_toast_on_capture.setFont(QFont('Arial', 10))
         self.g_settings_show_toast_on_capture.setStyleSheet('border: none;')
         self.g_settings_show_toast_on_capture.stateChanged.connect(self.cb_show_toast_on_capture_state_changed)
         self.g_settings_show_toast_on_capture.setChecked(CONFIG_DICT['show_toast_on_capture'])
         g_settings_layout.addWidget(self.g_settings_show_toast_on_capture)
 
-        # g_settings_layout.addStretch()
+        g_settings_layout.addStretch()
         self.g_settings.setLayout(g_settings_layout)
 
-        # 500x281
+        # Preview ------------------------------------------------------------------------------------------------------
         preview_layout = QVBoxLayout()
-        # preview_layout.addStretch(0)
+        preview_layout.setContentsMargins(0, 0, 0, 0)
         self.g_preview = QWidget(self)
         self.g_preview.setFixedHeight(400)
         self.g_preview.setFixedWidth(500)
@@ -162,12 +263,14 @@ class Window(QMainWindow):
         preview_layout.addWidget(self.preview_label)
 
         monitor_layout = QHBoxLayout()
+        monitor_layout.setContentsMargins(0, 0, 0, 0)
         self.g_monitors = QWidget(self)
         self.g_monitors.setMinimumHeight(20)
         self.g_monitors.setMinimumWidth(20)
 
         self.gen_monitor_list()
 
+        # Add monitor buttons ------------------------------------------------------------------------------------------
         for i in range(0, len(self.monitor_buttons)):
             monitor_layout.addWidget(self.monitor_buttons[i])
         monitor_layout.addStretch()
@@ -175,6 +278,7 @@ class Window(QMainWindow):
         self.g_monitors.setLayout(monitor_layout)
         preview_layout.addWidget(self.g_monitors)
 
+        # image --------------------------------------------------------------------------------------------------------
         self.image_preview = QWidget(self)
         self.image_preview.setFixedWidth(490)
         self.image_preview.setFixedHeight(281)
@@ -190,11 +294,22 @@ class Window(QMainWindow):
 
         self.g_preview.setLayout(preview_layout)
 
+        # Add Widgets to main widget -----------------------------------------------------------------------------------
         main_layout.addWidget(self.g_instructions)
         main_layout.addWidget(self.g_settings)
         main_layout.addWidget(self.g_preview)
         main_layout.addStretch()
         self.main_widget.setLayout(main_layout)
+
+        # Scrollbar ----------------------------------------------------------------------------------------------------
+        self.scroller = QScrollArea(self)
+        self.scroller.setWidget(self.main_widget)
+        self.scroller.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.scroller.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.scroller.setWidgetResizable(True)
+        self.scroller.setFixedWidth(APP_WIDTH)
+        self.scroller.setFixedHeight(APP_HEIGHT)
+        self.scroller.installEventFilter(self)
 
         self.show()
 
@@ -261,14 +376,23 @@ class Window(QMainWindow):
         CONFIG_DICT['show_toast_on_capture'] = self.g_settings_show_toast_on_capture.isChecked()
         update_config()
 
-    def closeEvent(self, event):
-        if os.path.exists(TEMP_IMAGE_PATH):
-            os.remove(TEMP_IMAGE_PATH)
+    def get_custom_hotkey(self):
+        pass
 
     def init_save_path(self):
         cwd = os.getcwd()
         CONFIG_DICT['ss_path'] = cwd
         update_config()
+
+    def closeEvent(self, event):
+        if os.path.exists(TEMP_IMAGE_PATH):
+            os.remove(TEMP_IMAGE_PATH)
+
+    def eventFilter(self, obj, event):
+        if event.type() == QtCore.QEvent.KeyPress:
+            print(event.key())
+
+        return super(Window, self).eventFilter(obj, event)
 
 
 with open(CONFIG_FILE) as config:
@@ -286,6 +410,7 @@ app_icon.addFile('./assets/icon-256x256.png', QtCore.QSize(256, 256))
 app.setWindowIcon(app_icon)
 
 window = Window()
+window.installEventFilter(window)
 start_gui()
 
 listener = threading.Thread(target=start_listening_for_hotkeys)
